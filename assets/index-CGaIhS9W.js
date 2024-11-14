ba77ab050ea322bb14a1ece216b997d9e19b325e
Miroslav Hájek <shrug-agent-trophy@duck.com>
2024-11-14 17:33:00 +0000

    add a site

---
+++ assets/index-CGaIhS9W.js
@@ -0,0 +1,6 @@
+(function(){const t=document.createElement("link").relList;if(t&&t.supports&&t.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))s(e);new MutationObserver(e=>{for(const r of e)if(r.type==="childList")for(const o of r.addedNodes)o.tagName==="LINK"&&o.rel==="modulepreload"&&s(o)}).observe(document,{childList:!0,subtree:!0});function i(e){const r={};return e.integrity&&(r.integrity=e.integrity),e.referrerPolicy&&(r.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?r.credentials="include":e.crossOrigin==="anonymous"?r.credentials="omit":r.credentials="same-origin",r}function s(e){if(e.ep)return;e.ep=!0;const r=i(e);fetch(e.href,r)}})();document.querySelector("#app").innerHTML=`
+  <div class="coming-soon">
+    <h1 class="name">Coming soon</h1>
+    <p class="subtitle">Check on my social media in the meantime</p>
+  </div>
+`;
